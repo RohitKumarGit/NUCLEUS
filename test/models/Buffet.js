78f37a6d52463cb4e1a401876d0a75239d331e53
Shantanu Goyal <shantanugoyal23@gmail.com>
2020-08-31 18:00:56 +0530

    Added Datebase Schema
    Added Database Schema. Rohit kindly check.

---
+++ test/models/Buffet.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose');
+const buffetSchema=new mongoose.Schema({
+  user_id:Number,
+  restaurant_id:Number,
+  available_slots:Array[{
+    slot_time:Array,
+    total_slots:Number,
+    available_slots:Number
+  }]
+});
+const Buffet=mongoose.model('Buffet',buffetSchema);
+module.exports=Buffet;
